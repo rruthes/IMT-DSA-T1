c86c2989581403fb00678cd92971866916e6e3ce
rruthes <rafaelruthes444@gmail.com>
2025-04-01 14:01:08 -0300

    NossoVetor.java

---
+++ NossoVetor.java
@@ -0,0 +1,103 @@
+import java.util.Random;
+public class NossoVetor {
+  private int ocupacao;
+  private int[] vetor;
+
+  public NossoVetor(int tamanho) {
+    vetor = new int[tamanho];
+    ocupacao = 0;
+  }
+
+  public NossoVetor() {
+    this(10);
+  }
+
+  public void inserir(int i) {
+    if (estaCheio())
+      redimensiona(vetor.length * 2);
+    vetor[ocupacao++] = i;
+  }
+  public int remover() {
+    if (estaVazio()) {
+        throw new VetorVazioException("vetor vazio, nao a o que remover");
+    }
+    int aux = vetor[--ocupacao];
+    if (vetor.length >= 6 && ocupacao <= vetor.length / 4) {
+        redimensiona(vetor.length / 2);
+    }
+    return aux;
+  }
+  public boolean estaCheio() {
+    return ocupacao == vetor.length;
+  }
+
+  public boolean estaVazio() {
+    return ocupacao == 0;
+  }
+  private void redimensiona(int novoTamanho) {
+    int[] temp = new int[novoTamanho];
+    for (int i = 0; i < ocupacao; i++) {
+      temp[i] = vetor[i];
+    }
+    vetor = temp;
+  }
+  public int getTamanho() {
+    return vetor.length;
+  }
+  public void preecheVetor () {
+    Random random = new Random();
+    for (int i=0; i<vetor.length; i++) {
+      vetor[i] = random.nextInt(vetor.length * 10);
+    }
+    ocupacao = vetor.length;
+  }
+  public boolean contem (int i) {
+    for (int j=0; j < ocupacao; j++)
+      if (vetor[j] == i)
+        return true;
+    return false;
+  }
+  public int indiceDe (int i) {
+    for (int j=0; j < ocupacao; j++)
+      if (vetor[j] == i)
+        return j;
+    return -1;
+  }
+  public void bubbleSort () {
+    for (int i=1; i<vetor.length; i++) {
+      for (int j=0; j < vetor.length - i; j++) {
+        if (vetor[j] > vetor[j+1]) {
+          int aux = vetor[j];
+          vetor[j] = vetor[j+1];
+          vetor[j+1] = aux;
+        }
+      }
+    }
+  }
+  public void selectionsort ()
+  {
+     for (int i = 0; i < vetor.length-1; ++i) {
+        int min = i;
+        for (int j = i+1; j < vetor.length; ++j)
+           if (vetor[j] < vetor[min])  min = j;
+        int x = vetor[i]; 
+        vetor[i] = vetor[min]; 
+        vetor[min] = x;
+     }
+  }
+  public void insertionsort ()
+  {
+     for (int j = 1; j < vetor.length; ++j) {
+        int x = vetor[j];
+        int i;
+        for (i = j-1; i >= 0 && vetor[i] > x; --i) 
+           vetor[i+1] = vetor[i];
+        vetor[i+1] = x;
+     }
+  }
+}
+class VetorVazioException extends RuntimeException {
+    public VetorVazioException(String msg) {
+      super(msg);
+    }
+}
