c12976374ae0fea5ee4dcf6c90bfc418106e02a4
rruthes <rafaelruthes444@gmail.com>
2025-04-11 16:22:26 -0300

    final version

--- TesteDesempenho.java
+++ TesteDesempenho.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 public class TesteDesempenho {
     public static void main(String[] args) {
 
-
         Scanner scanner = new Scanner(System.in);
         NossoVetor vetorTeste;
         int t;
